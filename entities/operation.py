c8e40fe5c2d770bebd6f8d7b3c41f232ae3d0bed
Галеев Ильдар <ildar-galeev92@mail.ru>
2023-08-08 15:46:45 +0700

    finished the Operation class
    added properties to the Operation

--- entities/operation.py
+++ entities/operation.py
@@ -1,5 +1,8 @@
 import re
 from collections import namedtuple
+from datetime import datetime
+
+from entities import operation_amount
 
 
 class Operation:
@@ -30,3 +33,59 @@ class Operation:
                     return f"XX {number_str[-4:]}"
 
         return None
+
+    def __init__(self, operation_details: dict):
+        self._id = operation_details.get("id", 0)
+        self._date = datetime.strptime(operation_details.get("date", ""), "%Y-%m-%dT%H:%M:%S.%f")
+        self._state = operation_details.get("state", None)
+        self._operation_amount = operation_amount.OperationAmount(operation_details.get("operationAmount", None))
+        self._description = operation_details.get("description", None)
+        self._from = Operation._parse_payment_details(operation_details.get("from", None))
+        self._to = Operation._parse_payment_details(operation_details.get("to", None))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(" \
+               f"id={self._id}," \
+               f"date={self._date},"\
+               f"state={self._state}," \
+               f"operationAmount={self._operation_amount}," \
+               f"description={self._description}," \
+               f"from={self._from}," \
+               f"to={self._to}" \
+               f")"
+
+    @property
+    def payment_from(self):
+        if self._from:
+            return f"{self._from.name} {Operation._stringify_number(self._from.number)}"
+        return None
+
+    @property
+    def payment_from_details(self) -> PaymentDetails | None:
+        return self._from
+
+    @property
+    def payment_to(self):
+        if self._to:
+            return f"{self._to.name} {Operation._stringify_number(self._to.number)}"
+        return None
+
+    @property
+    def payment_to_details(self) -> PaymentDetails | None:
+        return self._to
+
+    @property
+    def state(self) -> str:
+        return self._state
+
+    @property
+    def date(self) -> datetime:
+        return self._date
+
+    @property
+    def operation_amount(self) -> operation_amount.OperationAmount:
+        return self._operation_amount
+
+    @property
+    def description(self) -> str:
+        return self._description
--- tests/test_entities/test_operation.py
+++ tests/test_entities/test_operation.py
@@ -1,11 +1,12 @@
 import unittest
 from entities import Operation
+from entities import OperationAmount
+from datetime import datetime
 
 
 class OperationTestCase(unittest.TestCase):
 
     def test_operation__parse_bill_number_correct(self):
-
         result_bill_details = Operation._parse_payment_details("Счет 12345678901234567890")
         result_card_details = Operation._parse_payment_details("Some Card 1111222233334444")
 
@@ -16,7 +17,6 @@ class OperationTestCase(unittest.TestCase):
         self.assertEqual(result_card_details.name, "Some Card")
 
     def test_operation__parse_bill_number_invalid(self):
-
         result_1 = Operation._parse_payment_details("Счет 1234567890123")
         result_2 = Operation._parse_payment_details("")
 
@@ -24,16 +24,39 @@ class OperationTestCase(unittest.TestCase):
         self.assertIsNone(result_2)
 
     def test_operation__stringify_number_card_number(self):
-
         result = Operation._stringify_number("1357246833331234")
 
         self.assertEqual(result, "1357 24XX XXXX 1234")
 
     def test_operation__stringify_number_bill_number(self):
-
         result = Operation._stringify_number("12345678901234567890")
 
         self.assertEqual(result, "XX 7890")
+
+    def test_operation__all_properties_valid(self):
+        operation_item = {
+            "id": 1234,
+            "state": "EXECUTED",
+            "date": "2023-08-08T15:16:16.0",
+            "operationAmount": {
+                "amount": "123.45",
+                "currency": {
+                    "name": "cur.",
+                    "code": "CUR"
+                }
+            },
+            "description": "some description",
+            "from": "Some Card 1111222233334444",
+            "to": "Billnumber 11112222333344445555"
+        }
+
+        instance = Operation(operation_item)
+        self.assertEqual(instance.state, "EXECUTED")
+        self.assertEqual(instance.date, datetime(2023, 8, 8, 15, 16, 16))
+        self.assertEqual(instance.operation_amount, OperationAmount({"amount": "123.45", "currency": {"code": "CUR"}}))
+        self.assertEqual(instance.description, "some description")
+        self.assertEqual(instance.payment_from_details, ("Some Card", "1111222233334444"))
+        self.assertEqual(instance.payment_to_details, ("Billnumber", "11112222333344445555"))
 
 
 if __name__ == '__main__':
