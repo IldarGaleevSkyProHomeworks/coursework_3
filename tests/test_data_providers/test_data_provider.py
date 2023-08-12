77ea74c69a0f92dd75fd8c02458a9faedac1f267
Галеев Ильдар <ildar-galeev92@mail.ru>
2023-08-12 23:07:36 +0700

    changed to pytest: data_provider

--- tests/test_data_providers/test_data_provider.py
+++ tests/test_data_providers/test_data_provider.py
@@ -1,61 +1,66 @@
 import unittest.mock
 from datetime import datetime
 
+import pytest
+
 from coursework_3.data_providers import DataProvider
 
 
-class DataProviderTestCase(unittest.TestCase):
-    class DataProviderTestClass(DataProvider):
-        def __init__(self):
-            super().__init__()
-            self._operations = [
-                type('entities.Operation', (), {
-                    'date': datetime(2023, 1, 1),
-                    'state': 'EXECUTED',
-                    'payment_from_details': type('', (object,), {
-                        'name': 'Card',
-                        'number': '1111222233334441'
-                    }),
-                    'payment_to_details': type('', (object,), {
-                        'name': 'Card',
-                        'number': '1111222233334442'
-                    })}),
+class DataProviderTestClass(DataProvider):
+    """ Some DataProvider implementation """
+    def __init__(self):
+        super().__init__()
+        self._operations = [
+            type('entities.Operation', (), {
+                'date': datetime(2023, 1, 1),
+                'state': 'EXECUTED',
+                'payment_from_details': type('', (object,), {
+                    'name': 'Card',
+                    'number': '1111222233334441'
+                }),
+                'payment_to_details': type('', (object,), {
+                    'name': 'Card',
+                    'number': '1111222233334442'
+                })}),
 
-                type('entities.Operation', (), {
-                    'date': datetime(2023, 1, 2),
-                    'state': 'CANCELED',
-                    'payment_from_details': type('', (object,), {
-                        'name': 'Card',
-                        'number': '1111222233334441'
-                    }),
-                    'payment_to_details': type('', (object,), {
-                        'name': 'Card',
-                        'number': '1111222233334443'
-                    })}),
+            type('entities.Operation', (), {
+                'date': datetime(2023, 1, 2),
+                'state': 'CANCELED',
+                'payment_from_details': type('', (object,), {
+                    'name': 'Card',
+                    'number': '1111222233334441'
+                }),
+                'payment_to_details': type('', (object,), {
+                    'name': 'Card',
+                    'number': '1111222233334443'
+                })}),
 
-                type('entities.Operation', (), {
-                    'date': datetime(2023, 1, 3),
-                    'state': 'EXECUTED',
-                    'payment_from_details': type('', (object,), {
-                        'name': 'Card',
-                        'number': '1111222233334443'
-                    }),
-                    'payment_to_details': type('', (object,), {
-                        'name': 'Card',
-                        'number': '1111222233334442'
-                    })}),
-            ]
-
-    def test_data_provider_get_operations(self):
-        instance = DataProviderTestCase.DataProviderTestClass()
-        lst = instance.get_operations()
-        self.assertEqual(len(lst), 3)
-
-    def test_data_provider_get_account_operations(self):
-        instance = DataProviderTestCase.DataProviderTestClass()
-        lst = instance.get_operations_by_account_number('1111222233334441')
-        self.assertEqual(len(lst), 2)
+            type('entities.Operation', (), {
+                'date': datetime(2023, 1, 3),
+                'state': 'EXECUTED',
+                'payment_from_details': type('', (object,), {
+                    'name': 'Card',
+                    'number': '1111222233334443'
+                }),
+                'payment_to_details': type('', (object,), {
+                    'name': 'Card',
+                    'number': '1111222233334442'
+                })}),
+        ]
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture()
+def fixture_dataprovider_instance():
+    return DataProviderTestClass()
+
+
+def test_data_provider_get_operations(fixture_dataprovider_instance):
+    lst = fixture_dataprovider_instance.get_operations()
+
+    assert len(lst) == 3
+
+
+def test_data_provider_get_account_operations(fixture_dataprovider_instance):
+    lst = fixture_dataprovider_instance.get_operations_by_account_number('1111222233334441')
+
+    assert len(lst) == 2
